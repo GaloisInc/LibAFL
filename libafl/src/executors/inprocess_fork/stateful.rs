70d6554d19a5cdade205db955faabec75684c78c
Langston Barrett <langston.barrett@gmail.com>
2025-11-20 14:34:31 +0000

    Add `into_state` for stateful executors

--- libafl/src/executors/inprocess_fork/stateful.rs
+++ libafl/src/executors/inprocess_fork/stateful.rs
@@ -179,6 +179,13 @@ where
     pub fn harness_mut(&mut self) -> &mut H {
         self.harness_fn
     }
+
+    /// Retrieve the state, consuming the executor.
+    #[inline]
+    #[must_use]
+    pub fn into_state(self) -> ES {
+        self.exposed_executor_state
+    }
 }
 
 impl<H, HT, I, OT, S, SP, EM, ES, Z> HasObservers
